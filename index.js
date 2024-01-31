74d0e46bd4f52b1f5aaf8415c580d768559feecf
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-01-31 13:34:41 +0100

    Hover on button and changed some in html

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector('#wind-speed');
   let timeElement = document.querySelector('#time');
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
@@ -15,6 +16,7 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function searchCity(city) {
